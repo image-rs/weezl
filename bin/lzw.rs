784f06d32c7c3267c0af368fb7a3c89d8170afba
Andreas Molzer <andreas.molzer@gmx.de>
2020-06-25 12:40:09 +0200

    Implement a simplistic, full-buffered decode

--- bin/lzw.rs
+++ bin/lzw.rs
@@ -41,6 +41,37 @@ fn main() {
                 lzw::MsbWriter::new(out),
                 min_code)
         },
+        (Input::File(file), Operation::Decode) => (|| {
+            let data = fs::read(file)?;
+            let mut decoder = lzw::relzw::Decoder::new(lzw::relzw::ByteOrder::Msb, 8);
+            let mut outbuf = vec![0; 1 << 12];
+            let mut outmark = 0;
+            let mut data = data.as_slice();
+            loop {
+                let result = decoder.decode_bytes(data, &mut outbuf[outmark..]);
+                let done = result.status.map_err(|err| io::Error::new(
+                        io::ErrorKind::InvalidData, &*format!("{:?}", err)
+                    ))?;
+                data = &data[result.consumed_in..];
+                outmark += result.consumed_out;
+                if let lzw::relzw::LzwStatus::Done = done {
+                    break;
+                }
+                if let lzw::relzw::LzwStatus::NoProgress = done {
+                    if outmark == outbuf.len() {
+                        let next_len = outbuf.len() * 2;
+                        outbuf.resize(next_len, 0u8);
+                    } else {
+                        return Err(io::Error::new(
+                            io::ErrorKind::UnexpectedEof, ""
+                        ));
+                    }
+                }
+            }
+            outbuf.truncate(outmark);
+            io::copy(&mut outbuf.as_slice(), &mut {out})?;
+            Ok(())
+        })(),
         _ => unimplemented!("decoding is not implemented"),
     };
 
