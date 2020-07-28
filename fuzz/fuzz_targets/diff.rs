1c03dc3b4c2dfd1ef98b3c1f84c0db1c0ffd4428
Andreas Molzer <andreas.molzer@gmx.de>
2020-07-28 23:13:54 +0200

    Update diff fuzzing against lzw
    We do _not_ fuzz against the actual lzw crate since it doesn't allow us
    to reliable differentiate between a clear code and the end of stream.

--- fuzz/fuzz_targets/diff.rs
+++ fuzz/fuzz_targets/diff.rs
@@ -1,19 +1,22 @@
 #![no_main]
-use weezl::{Decoder, MsbReader, relzw};
 use libfuzzer_sys::fuzz_target;
 
-fuzz_target!(|data: &[u8]| {
+fuzz_target!(|raw_data: &[u8]| {
+    let mut data = vec![0b10];
+    data.extend_from_slice(raw_data);
+    let data = data.as_slice();
     const CUT_OFF: usize = 1 << 20;
 
-    let mut detailed_ref = None;
+    let mut detailed_ref: Option<_> = None;
     let reference = (|| {
-        let mut decoder = Decoder::new(MsbReader::new(), 8);
+        let mut decoder = lzw::Decoder::new(lzw::LsbReader::new(), 2);
         let mut data = data;
         let mut output = vec![];
         while !data.is_empty() && output.len() < CUT_OFF {
             match decoder.decode_bytes(data) {
                 Ok((0, _)) => break,
                 Ok((len, decoded)) => {
+                    // eprintln!("Ref {:?}", decoded);
                     data = &data[len..];
                     output.extend_from_slice(decoded);
                 }
@@ -33,31 +36,14 @@ fuzz_target!(|data: &[u8]| {
 
     let mut detailed_err = None;
     let new = (|| {
-        let mut decoder = weezl::relzw::Decoder::new(weezl::relzw::ByteOrder::Msb, 8);
-        let mut data = data;
-        let mut output = vec![0; CUT_OFF];
-        let mut out = output.as_mut_slice();
-        while !(data.is_empty() && out.is_empty()) {
-            let result = decoder.decode_bytes(data, out);
-            data = &data[result.consumed_in..];
-            out = &mut out[result.consumed_out..];
-
-            if let Err(err) = result.status {
-                detailed_err = Some(err);
-                return Err(());
-            }
-
-            if let Ok(relzw::LzwStatus::Done) = result.status {
-                break;
-            }
-
-            if let Ok(relzw::LzwStatus::NoProgress) = result.status {
-                break;
-            }
+        let mut decoder = weezl::decode::Decoder::new(weezl::BitOrder::Lsb, 2);
+        let mut output = Vec::with_capacity(CUT_OFF);
+        let err = decoder.into_stream(&mut output).decode(data);
+        if let Err(err) = err.status {
+            detailed_err = Some(err);
+            return Err(());
         }
-        let tail = out.len();
-        let len = output.len() - tail;
-        output.truncate(len);
+        output.truncate(CUT_OFF);
         Ok(output)
     })();
 
