c551ea80914a1ce77e8d3c22a57422f45be4e753
Andreas Molzer <andreas.molzer@gmx.de>
2020-11-10 18:44:51 +0100

    Collapse debugging code in roundtrips
    Removes the unnecessary writes to tmp that were useful for diffing the
    output but not much else.

--- tests/roundtrip.rs
+++ tests/roundtrip.rs
@@ -1,7 +1,7 @@
 use std::{env, fs};
 use weezl::{decode, encode, BitOrder};
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 enum Flavor {
     Gif,
     Tiff,
@@ -10,7 +10,6 @@ enum Flavor {
 #[test]
 fn roundtrip_all() {
     let file = env::args().next().unwrap();
-    dbg!(&file);
     let data = fs::read(file).unwrap();
 
     for &flavor in &[Flavor::Gif, Flavor::Tiff] {
@@ -22,6 +21,7 @@ fn roundtrip_all() {
                     .map(|b| b & ((1 << bit_width) - 1))
                     .collect();
 
+                println!("Roundtrip test {:?} {:?} {}", flavor, bit_order, bit_width);
                 assert_roundtrips(&*data, flavor, bit_width, bit_order);
             }
         }
@@ -39,19 +39,15 @@ fn assert_roundtrips(data: &[u8], flavor: Flavor, bit_width: u8, bit_order: BitO
             decode::Decoder::with_tiff_size_switch,
         ),
     };
-    eprintln!("Roundtrip test {:?} {}", bit_order, bit_width);
     let mut encoder = c(bit_order, bit_width);
     let mut buffer = Vec::with_capacity(2 * data.len() + 40);
     let _ = encoder.into_stream(&mut buffer).encode_all(data);
-    fs::write("/tmp/encoded", buffer.as_slice()).unwrap();
 
     let mut decoder = d(bit_order, bit_width);
     let mut compare = vec![];
     let result = decoder
         .into_stream(&mut compare)
         .decode_all(buffer.as_slice());
-    fs::write("/tmp/decoded0", data).unwrap();
-    fs::write("/tmp/decoded1", compare.as_slice()).unwrap();
     assert!(
         result.status.is_ok(),
         "{:?}, {}, {:?}",
